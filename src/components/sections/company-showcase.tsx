c11a59aa84c5618f069e57df065e302f34f2f6a5
David Muehlenweg <services@avorice.de>
2025-08-01 17:16:53 +0200

    kjj

--- src/components/sections/company-showcase.tsx
+++ src/components/sections/company-showcase.tsx
@@ -15,7 +15,7 @@ export function CompanyShowcase() {
       <div className="grid w-full max-w-7xl grid-cols-2 md:grid-cols-4 overflow-hidden border-y border-border items-center justify-center z-20">
         {companyShowcase.companyLogos.map((logo) => (
           <Link
-            href="#"
+            href="https://app.finseo.ai/signup"
             className="group w-full h-28 flex items-center justify-center relative p-4 before:absolute before:-left-1 before:top-0 before:z-10 before:h-screen before:w-px before:bg-border before:content-[''] after:absolute after:-top-1 after:left-0 after:z-10 after:h-px after:w-screen after:bg-border after:content-['']"
             key={logo.id}
           >
